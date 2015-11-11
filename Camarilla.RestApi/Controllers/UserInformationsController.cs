86d902c6254a42fd42aab18696807fdcab1b8e51
Philippe Matray <phmatray@outlook.com>
2015-11-11 23:38:30 +0100

    Get a fake userInformation

--- Camarilla.RestApi/Controllers/UserInformationsController.cs
+++ Camarilla.RestApi/Controllers/UserInformationsController.cs
@@ -31,5 +31,29 @@ namespace Camarilla.RestApi.Controllers
 
             return Ok(userInformations);
         }
+
+        /// <summary>
+        ///     Get a fake user information
+        /// </summary>
+        /// <remarks>
+        ///     Get an object of a fake object userInformation
+        /// </remarks>
+        /// <response code="200">OK</response>
+        /// <response code="500">Internal Server Error</response>
+        [Route("sample")]
+        [ResponseType(typeof (UserInformation))]
+        public IHttpActionResult GetSample()
+        {
+            var userInformation = new UserInformation()
+            {
+                Id = -1,
+                FirstName = "Philippe",
+                LastName = "Matray",
+                Birthday = new DateTime(1988, 8, 1),
+                Gender = Gender.Male
+            };
+
+            return Ok(userInformation);
+        }
     }
 }
