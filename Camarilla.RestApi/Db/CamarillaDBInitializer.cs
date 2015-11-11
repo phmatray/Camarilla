1d30568d9a98e885bde6337a3463dab92113ba5f
Philippe Matray <phmatray@outlook.com>
2015-11-11 15:37:14 +0100

    Updated CamarillaDbInitializer

--- Camarilla.RestApi/Db/CamarillaDBInitializer.cs
+++ Camarilla.RestApi/Db/CamarillaDBInitializer.cs
@@ -1,31 +1,37 @@
+using System;
 using System.Data.Entity;
+using Camarilla.RestApi.Managers;
+using Camarilla.RestApi.Models;
+using Camarilla.RestApi.Stores.Concretes;
+using Microsoft.AspNet.Identity;
 
 namespace Camarilla.RestApi.Db
 {
-    public class CamarillaDBInitializer : DropCreateDatabaseAlways<CamarillaContext>
+    public class CamarillaDbInitializer : DropCreateDatabaseAlways<CamarillaContext>
     {
         protected override void Seed(CamarillaContext context)
         {
-            //var userManager = new CamarillaUserManager(new CamarillaUserStore(context));
+            var userStore = new CamarillaUserStore(context);
+            var userManager = new CamarillaUserManager(userStore);
 
-            //var userInformation = new UserInformation
-            //{
-            //    Birthday = new DateTime(1988, 8, 1),
-            //    FirstName = "Philippe",
-            //    LastName = "Matray",
-            //    Gender = Gender.Male
-            //};
+            var userInformation = new UserInformation
+            {
+                Birthday = new DateTime(1988, 8, 1),
+                FirstName = "Philippe",
+                LastName = "Matray",
+                Gender = Gender.Male
+            };
 
-            //var camarillaUser = new CamarillaUser("phmatray")
-            //{
-            //    Email = "phmatray@gmail.com",
-            //    PhoneNumber = "0032473322929",
-            //    UserInformation = userInformation
-            //};
+            var camarillaUser = new CamarillaUser("phmatray")
+            {
+                Email = "phmatray@gmail.com",
+                PhoneNumber = "0032473322929",
+                UserInformation = userInformation
+            };
 
-            //userManager.Create(camarillaUser, "camarilla");
+            userManager.Create(camarillaUser, "camarilla");
 
-            //base.Seed(context);
+            base.Seed(context);
         }
     }
 }
