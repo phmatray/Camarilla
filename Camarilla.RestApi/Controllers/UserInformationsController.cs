167ae2f2b9bafd1f4c9ebb36d6ee2cbfacdf2297
Philippe Matray <phmatray@outlook.com>
2015-11-11 15:50:38 +0100

    add UserInformationsController

---
+++ Camarilla.RestApi/Controllers/UserInformationsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Camarilla.RestApi.Db;
+using Camarilla.RestApi.Models;
+using Camarilla.RestApi.Stores.Concretes;
+
+namespace Camarilla.RestApi.Controllers
+{
+    [Route("api/userInformations")]
+    public class UserInformationsController : ApiController
+    {
+        // GET: api/userInformations
+        [HttpGet]
+        public IEnumerable<UserInformation> Get()
+        {
+            var context = new CamarillaContext();
+            var store = new UserInformationStore(context);
+
+            var userInformations = store.UserInformations.ToList();
+            return userInformations;
+        }
+    }
+}
