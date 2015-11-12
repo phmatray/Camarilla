20d0bbb50e291a6ce8820b60b671fba5470260a6
Philippe Matray <phmatray@outlook.com>
2015-11-12 21:12:12 +0100

    Get a user from database by its username

--- Camarilla.RestApi/Controllers/UsersController.cs
+++ Camarilla.RestApi/Controllers/UsersController.cs
@@ -22,8 +22,9 @@ namespace Camarilla.RestApi.Controllers
         /// </remarks>
         /// <response code="200">OK</response>
         /// <response code="500">Internal Server Error</response>
+        [HttpGet]
         [Route("")]
-        [ResponseType(typeof(List<User>))]
+        [ResponseType(typeof (List<User>))]
         public IHttpActionResult Get()
         {
             var context = new CamarillaContext();
@@ -35,6 +36,39 @@ namespace Camarilla.RestApi.Controllers
         }
 
         /// <summary>
+        ///     Get a user from DB
+        /// </summary>
+        /// <param name="username">
+        ///     Email
+        /// </param>
+        /// <remarks>
+        ///     Get a user from database by its username
+        /// </remarks>
+        /// <response code="200">OK</response>
+        /// <response code="400">Bad request</response>
+        /// <response code="404">Not found</response>
+        /// <response code="500">Internal Server Error</response>
+        [HttpGet]
+        [Route("{username}")]
+        [ResponseType(typeof(User))]
+        public IHttpActionResult GetByUsername(string username)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+                return BadRequest("username cannot be null or white space");
+
+            var context = new CamarillaContext();
+            var store = new UserStore(context);
+            var manager = new UserManager(store);
+
+            var user = manager.FindByName(username);
+
+            if (user == null)
+                return NotFound();
+
+            return Ok(user);
+        }
+
+        /// <summary>
         ///     Get a fake user
         /// </summary>
         /// <remarks>
@@ -42,8 +76,9 @@ namespace Camarilla.RestApi.Controllers
         /// </remarks>
         /// <response code="200">OK</response>
         /// <response code="500">Internal Server Error</response>
+        [HttpGet]
         [Route("sample")]
-        [ResponseType(typeof(User))]
+        [ResponseType(typeof (User))]
         public IHttpActionResult GetSample()
         {
             var userInformation = new User
@@ -61,12 +96,13 @@ namespace Camarilla.RestApi.Controllers
         ///     Get a fake user from DB
         /// </summary>
         /// <remarks>
-        ///     Get an object of a fake object user from database
+        ///     Get a fake object that represents a user from database
         /// </remarks>
         /// <response code="200">OK</response>
         /// <response code="500">Internal Server Error</response>
+        [HttpGet]
         [Route("sampledb")]
-        [ResponseType(typeof(User))]
+        [ResponseType(typeof (User))]
         public IHttpActionResult GetSampleFromDb()
         {
             var context = new CamarillaContext();
